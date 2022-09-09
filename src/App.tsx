e88faebe9f2adb84fdbb75870c4e8975e9e4ac15
Ilke Kaya <ilkekaya@Ilkes-Mac-mini.local>
2022-09-09 08:10:54 -0700

    fix profile photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import profile from './profile.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={profile} className="Profile-photo" alt="profile photo" />
+        <img src="/profile.png" className="Profile-photo" alt="profile photo" />
       </header>
     </div>
   );
